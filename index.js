2dbf1105436c6c024aa90419ea515cc8ae4dfd7f
cmarcellus1125 <chrismarcelluspom@gmail.com>
2023-01-30 22:47:21 -0700

    Fix attempt for accordion coding

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 jQuery(function ($) {
-    $('.btn').click(function () {
+    $('.btn').click(function (e) {
+        e.preventDefault()
         $(this).toggleClass('open')
-        $(this).find('.arrow').toggleClass('open');
+        $(this).find('.arrow').toggleClass('flipped');
+        if ($(this).hasClass('flipped')){
+            $(this).removeClass('flipped')
+        }
+    })
+})
+jQuery(function ($) {
+    $('.btn').blur(function () {
+        $(this).find('.arrow').toggleClass('flipped');
     })
 })
